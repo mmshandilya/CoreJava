a90670cd163559b387a8639fe65e869b9b8cab32
LENOVO <LENOVO@DESKTOP-5JEGIAJ>
2022-03-21 17:45:25 +0530

    Today's commit 21 March

---
+++ CoreJava/src/net/java8/LambdaExample.java
@@ -0,0 +1,23 @@
+package net.java8;
+
+public class LambdaExample {
+
+	public static void main(String[] args) {
+	
+//	MyInter inter=new MyInter(){
+//		public int sum(int a,int b)
+//		{
+//			return a+b;
+//		}
+//		
+//	};	
+//	MyInter inter=(a,b)->{
+//			return a+b;
+//	};	
+		
+	MyInter inter=(a,b)->a+b;
+	
+	System.out.println(inter.sum(2, 3));
+	}
+
+}
