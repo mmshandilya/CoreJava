b25b77eb957885e12e4fbfa3f46751c166357c65
LENOVO <LENOVO@DESKTOP-5JEGIAJ>
2022-07-14 14:21:48 +0530

    Todays

--- CoreJava/src/OverlOverr.java
+++ CoreJava/src/OverlOverr.java
@@ -2,6 +2,7 @@ class Charges
 {
 	void deleteCharges (String s) throws ArithmeticException
 	{
+		
 		System.out.println("in Delete Charges");
 	}
 }
@@ -22,10 +23,12 @@ public class OverlOverr extends Charges
 		System.out.println("in sub class add charges with 2 double parameter");
 		return i+j;
 	}
-	public void deleteCharges(String s) throws ArithmeticException
+
+	public void deleteCharges(String s) throws ArithmeticException 
 	{
-		System.out.println("in sub class delete charges");
+		  System.out.println("in sub class delete charges"); 
 	}
+	 
 	public static void main(String args[])
 	{
 		OverlOverr o=new OverlOverr();
