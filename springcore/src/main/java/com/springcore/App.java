4627901b50b674c30a85a008b768766f5ff339ac
LENOVO <LENOVO@DESKTOP-5JEGIAJ>
2022-09-13 15:08:02 +0530

    13 Sept

--- springcore/src/main/java/com/springcore/App.java
+++ springcore/src/main/java/com/springcore/App.java
@@ -1,7 +1,13 @@
 package com.springcore;
 
+
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 
 /**
  * Hello world!
@@ -9,11 +15,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App 
 {
+	private static final Logger logger = LogManager.getLogger(App.class);
     public static void main( String[] args )
     {
 //    		Student student=new Student();
 //        	student.setStudentName("Mahesh");
-
+    	//logger.trace("We've just greeted the user!");
+       // logger.debug("We've just greeted the user!");
+        logger.info("We've just greeted the user!");
+        //logger.warn("We've just greeted the user!");
+        //logger.error("We've just greeted the user!");
+        //logger.fatal("We've just greeted the user!");
+        
     		ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
     		Student student=(Student) context.getBean("Student1");
         	System.out.println(student);
