8477c773f6a4cbc63b089f484e91b114136d93ce
mmshandilya <maheshmshandilya@gmail.com>
2022-11-03 10:00:52 +0530

    03 Nov

--- CoreJava/src/SortExample.java
+++ CoreJava/src/SortExample.java
@@ -5,9 +5,9 @@ public class SortExample {
 	public static void main(String [] arr)
 	{
 		ArrayList<Student> std=new ArrayList<>();
-		std.add(new Student("Mahesh","Mumbai",10));
+		std.add(new Student("Pravin","Mumbai",10));
 		std.add(new Student("Atul","Pune",9));
-		std.add(new Student("Deepak","AKola",15));
+		std.add(new Student("Shirpad","AKola",15));
 		System.out.println(""+std);
 		Collections.sort(std);//Comparable
 		System.out.println("Comparable: "+std);
